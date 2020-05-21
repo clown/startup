f24ee90226935e476fa9f0708ac32698532de31b
clown <tt.clown@gmail.com>
2020-05-21 17:02:29 +0900

    Fix startup scripts.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,14 @@
 cd `dirname $0`
 yum update -y
 
+# settings
+cp -pf etc/sysctl.conf /etc/sysctl.conf
+sysctl -p
+
+cp -pf etc/sshd_config /etc/ssh/sshd_config
+service sshd restart
+
+# others
 source install/tools.sh
 source install/docker.sh
 source install/ruby.sh
